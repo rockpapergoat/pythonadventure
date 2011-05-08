42aec60d8c4e754e85b62f78f5d972d303dbcecf
nate <n8@adullmoment.com>
2011-05-08 19:12:26 -0400

    added options and not_working function

--- code/booklist.py
+++ code/booklist.py
@@ -12,16 +12,13 @@ outline
 
 '''
 
-
-catalog = []
-
-
 def usage():
 	"""print usage"""
-	pass
+	print('The menu describes usage. Please type a number.')
+	
 
 def menu():
-	answer = input('\n--------------------------\nWhat would you like to do?\n--------------------------\n1. Add books\n2. Remove books\n3. Search for books\n4. Edit an entry\n5. Quit\n\n')
+	answer = input('\n--------------------------\nWhat would you like to do?\n--------------------------\n1. Add books\n2. Remove books\n3. Search for books\n4. Edit an entry\n5. View all books\n6. Quit\n\n')
 	return answer.rstrip()
 
 def add_books():
@@ -29,34 +26,63 @@ def add_books():
 	'''title, author, pages, isbn, category'''
 	entry = input('Give me some book details in this format: Title, Author, Pages, ISBN, Category\n')
 	if len(entry) != 0:
-		print(entry.split(','))
+		newbook = (entry.split(','))
+		details = ['title','author','pages','isbn','category']
+		catalog.update(dict(zip(details,newbook)))
+		print(catalog)
 	else:
-		print('You need to pass me a book.')
-	pass
+		print('You need to  me a book.')
+
 
 def append_item():
 	"""append parameters to already entered records"""
-	pass
+	
 
 def remove_books():
-	pass
+	'''method to remove books from catalog dict'''
+
+def show_catalog():
+	"""show what's in the catalog now"""
+	print('Your catalog includes:\n')
+	#for (key,value) in catalog:
+	#	print(catalog[key,value])
+	print(sorted(catalog))
 
 def find_books():
 	"""method to look up books in catalog"""
-	pass
+	
 
+def not_working():
+	"""output something in place of methods that don't exist yet"""
+	print("Yo, this isn't working yet.")
+	
 
 	
 """present a menu to choose tasks"""
-choice = menu()
-if choice == '1':
-	add_books()
-elif choice == '2':
-	print("Yo, this isn't working yet.")
-elif choice == '3':
-	print("Yo, this isn't working yet.")
-elif choice == '4':
-	print("Yo, this isn't working yet.")
-elif choice == '5':
-	print('Thanks for playing. See you next time.')
-	exit(0)
+
+catalog = {}
+loopcount = 0
+while loopcount == 0:
+	choice = menu()
+	if choice == '1':
+		add_books()
+		menu()
+	elif choice == '2':
+		not_working()
+		menu()
+	elif choice == '3':
+		not_working()
+		menu()
+	elif choice == '4':
+		not_working()
+		menu()
+	elif choice == '5':
+		show_catalog()
+		menu()
+	elif choice == '6':
+		print('Thanks for playing. See you next time.')
+		loopcount = 1
+		exit(0)
+	else:
+		usage()
+		menu()
